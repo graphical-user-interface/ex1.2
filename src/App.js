8ed210192046dc0dc9a6f835f0997fff9f93bfa1
thang241190 <thangnguyen24111990@gmail.com>
2021-03-16 12:26:05 +0200

    Finish ex1.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import {
+	Grid,
+	Container,
+	Checkbox,
+	Paper,
+	TextField,
+	Button
+} from "@material-ui/core"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1
+	},
+	paper: {
+		margin: theme.spacing(2),
+		padding: theme.spacing(2, 1),
+		textAlign: "center",
+		color: theme.palette.text.secondary
+	},
+	row: {
+		padding: theme.spacing(0.5, 2),
+		textAlign: "center"
+	},
+	input: {
+		height: 2
+	}
+}))
+
+export default function App() {
+	const [binary, setBinary] = useState([0, 0, 0, 0, 0, 0, 0, 0])
+	const handleChange = (e) => {
+		const newBinary = [...binary]
+		newBinary[e.target.value] = e.target.checked ? 1 : 0
+		setBinary(newBinary)
+	}
+
+	const renderCheckbox = () => {
+		const html = []
+		let i
+		for (i = 0; i < 8; i++) {
+			html.push(
+				<Checkbox
+					onChange={handleChange}
+					inputProps={{
+						"aria-label": "primary checkbox"
+					}}
+					key={i}
+					value={i}
+				/>
+			)
+		}
+
+		return html
+	}
+
+	const classes = useStyles()
+	const [convertedValue, setConvertedValue] = useState(0)
+	const convertBinary = () => {
+		const convertedNum = parseInt(binary.join(""), 2)
+		setConvertedValue(convertedNum)
+	}
+	return (
+		<div className={classes.root}>
+			<Container maxWidth='sm'>
+				<Grid container>
+					<Grid item xs={2}></Grid>
+					<Grid item xs={8}>
+						<Paper elevation={2}>
+							<Grid container className={classes.row}>
+								{renderCheckbox()}
+							</Grid>
+							<Grid container className={classes.row}>
+								<Grid item xs={12}>
+									<TextField
+										value={binary.join("")}
+										variant='outlined'
+										InputProps={{
+											classes: {
+												input: classes.input
+											}
+										}}
+									/>
+								</Grid>
+							</Grid>
+							<Grid container className={classes.row}>
+								<Grid item xs={12}>
+									<Button
+										variant='contained'
+										color='primary'
+										onClick={convertBinary}>
+										Convert
+									</Button>
+								</Grid>
+							</Grid>
+							<Grid container className={classes.row}>
+								<Grid item xs={12}>
+									<TextField
+										value={convertedValue.toString()}
+										variant='outlined'
+										InputProps={{
+											classes: {
+												input: classes.input
+											}
+										}}
+									/>
+								</Grid>
+							</Grid>
+						</Paper>
+					</Grid>
+					<Grid item xs={2}></Grid>
+				</Grid>
+			</Container>
+		</div>
+	)
 }
-
-export default App;
